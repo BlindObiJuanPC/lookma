519d6196809e94d47dbe10c1fd0ba3d0266ff791
Jordan Jones <riggingranger@gmail.com>
2025-12-31 05:23:04 -0800

    Have torch recompiles show up in the logs

--- scripts/train.py
+++ scripts/train.py
@@ -343,4 +343,8 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
+    if args.compile:
+        # User requested constant feedback on recompilation
+        os.environ["TORCH_LOGS"] = "recompiles"
+
     train(args)
